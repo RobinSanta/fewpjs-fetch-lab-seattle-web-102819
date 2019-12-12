5bd74e0bbbd10673b41ac6e4b1771603b03bded7
RobinSanta <robinsntn@gmail.com>
2019-12-12 15:43:47 -0800

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
 }
 
 function renderBooks(json) {
@@ -8,9 +10,9 @@ function renderBooks(json) {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
-  })
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
-})
+});
